4ee849479efd41258e8d19d8427104628687d5a6
lakshankd <dominiclakshank@gmail.com>
2023-04-04 21:11:11 +0530

    adding back-end

---
+++ src/main/java/com/websafe/ccmsbe/CcmsBeApplication.java
@@ -0,0 +1,13 @@
+package com.websafe.ccmsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcmsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcmsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websafe/ccmsbe/CcmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websafe.ccmsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcmsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
